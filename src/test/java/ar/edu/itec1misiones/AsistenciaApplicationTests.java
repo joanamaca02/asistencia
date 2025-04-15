bef45ee16573b028fdcb462afd766df1243ec05f
Joana Macarena Rios <103287225+joanamaca02@users.noreply.github.com>
2025-04-15 20:02:26 -0300

    feat: initial commit

---
+++ src/test/java/ar/edu/itec1misiones/AsistenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.itec1misiones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsistenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
