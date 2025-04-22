1d4e79fb581dc1ba8f9af00ffc10831b4b3f0ab6
Joana Macarena Rios <103287225+joanamaca02@users.noreply.github.com>
2025-04-22 12:21:50 -0300

    prueba2

---
+++ src/main/java/ar/edu/itec1misiones/dto/request/SaludoRequest.java
@@ -0,0 +1,12 @@
+package ar.edu.itec1misiones.dto.request;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Setter
+@Getter
+public class SaludoRequest {
+    private String message;
+}
---
+++ src/main/java/ar/edu/itec1misiones/dto/response/SaludoResponse.java
@@ -0,0 +1,12 @@
+package ar.edu.itec1misiones.dto.response;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Setter
+@Getter
+public class SaludoResponse {
+    private String message;
+}
